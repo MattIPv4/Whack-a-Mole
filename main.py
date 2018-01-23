72637b8af6066f7f37f0c454884aba5d7437ce04
MattIPv4 <matthew@cowley.org.uk>
2018-01-23 21:16:18 +0000

    generate holes

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ from enum import Enum
 import pygame
 
 
-class GameConstants(Enum):
+class GameConstants:
     """
     Stores all the constants used in the game
     """
-    WIDTH   = 500
-    HEIGHT  = 750
+    # Game Size
+    GAMEWIDTH   = 500
+    GAMEHEIGHT  = 750
+
+    # Hole Size
+    HOLEWIDTH  = int(GAMEWIDTH*0.25)
+    HOLEHEIGHT  = int(HOLEWIDTH*(3/8))
+
+    # Mole Size
+    MOLEWIDTH   = int(HOLEWIDTH*(2/3))
+    MOLEHEIGHT  = int(MOLEWIDTH)
+
+    # Holes
+    HOLEROWS    = 4
+    HOLECOLUMNS = 3
+
+    # PyGame Button Values
+    LEFTMOUSEBUTTON = 1
+
+    # Misc Data
     TITLE   = "Whack a Mole"
 
 
@@ -29,5 +47,63 @@ class Game:
 
     def __init__(self):
         # Create pygame screen
-        self.screen = pygame.display.set_mode((GameConstants.WIDTH, GameConstants.HEIGHT))
+        self.screen = pygame.display.set_mode((GameConstants.GAMEWIDTH, GameConstants.GAMEHEIGHT))
         pygame.display.set_caption(GameConstants.TITLE)
+
+        # Load background
+        self.img_background = pygame.image.load("background.png")
+        self.img_background = pygame.transform.scale(self.img_background, (GameConstants.GAMEWIDTH, GameConstants.GAMEHEIGHT))
+
+        # Load hole
+        self.img_hole = pygame.image.load("hole.png")
+        self.img_hole = pygame.transform.scale(self.img_hole, (GameConstants.HOLEWIDTH, GameConstants.HOLEHEIGHT))
+
+        # Generate hole positions
+        self.holes = []
+        base_row = GameConstants.GAMEHEIGHT/GameConstants.HOLEROWS
+        base_column = GameConstants.GAMEWIDTH/GameConstants.HOLECOLUMNS
+        for row in range(GameConstants.HOLEROWS):
+            rowY = base_row * row
+            rowY += (base_row-GameConstants.HOLEHEIGHT)/2
+            for column in range(GameConstants.HOLECOLUMNS):
+                thisX =  base_column * column
+                thisX += (base_column-GameConstants.HOLEWIDTH)/2
+                self.holes.append((int(thisX), int(rowY)))
+
+    def get_mole_position(self, hole_num):
+        holeX, holeY = self.holes[hole_num]
+
+    def start(self):
+        self.clock = pygame.time.Clock()
+        self.loop = True
+
+        while self.loop:
+
+            # Handle PyGame Events
+            for event in pygame.event.get():
+
+                # Handle quit
+                if event.type == pygame.QUIT:
+                    self.loop = False
+                    continue
+
+                # Handle click
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == GameConstants.LEFTMOUSEBUTTON:
+                    pass
+                    # TODO: Do collision checks
+
+            self.screen.blit(self.img_background, (0, 0))
+            for position in self.holes:
+                self.screen.blit(self.img_hole, position)
+
+            # Update display
+            pygame.display.flip()
+
+
+    def run(self):
+        pygame.init()
+        self.start()
+        pygame.quit()
+
+theGame = Game()
+theGame.run()
