a3a6b6264b64b6256f4cbdf95531d1d40dcfc9cb
MattIPv4 <matthew@cowley.org.uk>
2018-01-23 20:31:25 +0000

    lets start

---
+++ main.py
@@ -0,0 +1,33 @@
+from enum import Enum
+import pygame
+
+
+class GameConstants(Enum):
+    """
+    Stores all the constants used in the game
+    """
+    WIDTH   = 1000
+    HEIGHT  = 750
+    TITLE   = "Whack a Mole"
+
+
+class Score:
+    """
+    Handles the scoring for the player
+    """
+
+    def __init__(self):
+        self.score = 0
+        self.misses = 0
+        self.level = 1
+
+
+class Game:
+    """
+    Handles the main game
+    """
+
+    def __init__(self):
+        # Create pygame screen
+        self.screen = pygame.display.set_mode((GameConstants.WIDTH, GameConstants.HEIGHT))
+        pygame.display.set_caption(GameConstants.TITLE)
